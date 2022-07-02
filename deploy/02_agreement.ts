682f63bfce31b8f8c2309dd47deef5a2dea301b8
Yaghoub Adelzadeh <adelzadeh74@gmail.com>
2022-07-02 10:40:40 +0430

    first commit

---
+++ deploy/02_agreement.ts
@@ -0,0 +1,10 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import config from 'config'
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+ 
+}
+
+export default func
+func.tags = ["Agreement"]
