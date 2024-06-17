9c24693e7b6b3c14b3ba4491f431675edd6d4d87
Yaghoub Adelzadeh <adelzadeh74@gmail.com>
2024-06-17 16:01:31 +0330

    Update IERC20Metadata.sol

--- contracts/ERC20/extensions/IERC20Metadata.sol
+++ contracts/ERC20/extensions/IERC20Metadata.sol
@@ -1,27 +1,24 @@
 // SPDX-License-Identifier: MIT
+// Author: Yaghoub Adelzadeh
+// GitHub: https://www.github.com/dappteacher
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.25;
 
 import "../IERC20.sol";
 
-/**
- * @dev Interface for the optional metadata functions from the ERC20 standard.
- *
- * _Available since v4.1._
- */
+// @title IERC20Metadata Interface
+// @dev Interface for the ERC20 token with metadata.
 interface IERC20Metadata is IERC20 {
-    /**
-     * @dev Returns the name of the token.
-     */
+
+    // @notice Get the name of the token.
+    // @return The name of the token.
     function name() external view returns (string memory);
 
-    /**
-     * @dev Returns the symbol of the token.
-     */
+    // @notice Get the symbol of the token.
+    // @return The symbol of the token.
     function symbol() external view returns (string memory);
 
-    /**
-     * @dev Returns the decimals places of the token.
-     */
+    // @notice Get the number of decimals for the token.
+    // @return The number of decimals for the token.
     function decimals() external view returns (uint8);
 }
