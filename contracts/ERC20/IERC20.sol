431a4532858cafc32ed3717b69f8961aa0847b06
Yaghoub Adelzadeh <adelzadeh74@gmail.com>
2024-06-19 09:34:19 +0330

    Update IERC20.sol

--- contracts/ERC20/IERC20.sol
+++ contracts/ERC20/IERC20.sol
@@ -6,72 +6,49 @@ pragma solidity ^0.8.25;
 
 interface IERC20 {
 
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
+    // @notice Emitted when `value` tokens are moved from one account (`from`) to another (`to`)
+    // @param from The address of the sender
+    // @param to The address of the receiver
+    // @param value The amount of tokens transferred
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
+    // @notice Emitted when the allowance of a `spender` for an `owner` is set by a call to `approve`
+    // @param owner The address of the token owner
+    // @param spender The address of the spender
+    // @param value The new allowance amount
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    /**
-     * @dev Returns the amount of tokens in existence.
-     */
+    // @notice Returns the amount of tokens in existence
+    // @return The total supply of tokens
     function totalSupply() external view returns (uint256);
 
-    /**
-     * @dev Returns the amount of tokens owned by `account`.
-     */
+    // @notice Returns the amount of tokens owned by `account`
+    // @param account The address to query the balance of
+    // @return The balance of the account
     function balanceOf(address account) external view returns (uint256);
 
-    /**
-     * @dev Moves `amount` tokens from the caller's account to `to`.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
+    // @notice Moves `amount` tokens from the caller's account to `to`
+    // @param to The address of the recipient
+    // @param amount The amount of tokens to be transferred
+    // @return A boolean value indicating whether the operation succeeded
     function transfer(address to, uint256 amount) external returns (bool);
 
-    /**
-     * @dev Returns the remaining number of tokens that `spender` will be
-     * allowed to spend on behalf of `owner` through {transferFrom}. This is
-     * zero by default.
-     *
-     * This value changes when {approve} or {transferFrom} are called.
-     */
+    // @notice Returns the remaining number of tokens that `spender` will be allowed to spend on behalf of `owner` through `transferFrom`
+    // @param owner The address of the token owner
+    // @param spender The address of the spender
+    // @return The remaining allowance for the spender
     function allowance(address owner, address spender) external view returns (uint256);
 
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
-     * that someone may use both the old and the new allowance by unfortunate
-     * transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the
-     * desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     * Emits an {Approval} event.
-     */
+    // @notice Sets `amount` as the allowance of `spender` over the caller's tokens
+    // @param spender The address of the spender
+    // @param amount The amount of tokens to be approved
+    // @return A boolean value indicating whether the operation succeeded
     function approve(address spender, uint256 amount) external returns (bool);
 
-    /**
-     * @dev Moves `amount` tokens from `from` to `to` using the
-     * allowance mechanism. `amount` is then deducted from the caller's
-     * allowance.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
+    // @notice Moves `amount` tokens from `from` to `to` using the allowance mechanism
+    // @param from The address of the sender
+    // @param to The address of the recipient
+    // @param amount The amount of tokens to be transferred
+    // @return A boolean value indicating whether the operation succeeded
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
 }
